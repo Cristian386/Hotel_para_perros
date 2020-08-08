72ffe6b3777eb9c37e9218c303f53a887399c843
Cristian Flores <al221910343@gmail.com>
2020-08-08 10:06:31 -0500

    Diseño inicio sesion

--- index.php
+++ index.php
@@ -7,23 +7,24 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/all.min.css">
 </head>
-<body background="img/musica.jpg" class="img-fluid" alt="Responsive image">
+<body background="img/musica.jpg" class="card bg-dark text-white" alt="Responsive image">
     <div class="container mt-3">
-        <div class="card">
+        <div class="card-img-overlay">
             <div class="card-header">
-                <i class="fa fa-heart"></i> Inicio de sesion
+                <i class="fa fa-heart"></i> <h1>Inicio de sesion</h1>
             </div>
             <div class="card-body">  
                 <form action="inicio_sesion.php" method="post">
                     <div class="form-group">
-                        <label for="correo">Correo electronico</label>
+                        <label for="correo"><h5>Correo electronico </h5></label>
                         <input type="email" class="form-control form-control-sm" id="correo" name="correo" required>
                     </div>
                     <div class="form-group">
-                        <label for="contrasena">Contraseña</label>
+                        <label for="contrasena"><h5>Contraseña<h5></label>
                         <input type="password" class="form-control form-control-sm" id="contrasena" name="contrasena" required>
                     </div>
                     <button class="btn btn-primary btn-sm" type="submit"><i class="fa fa-save"></i> Enviar</button>
+                    <a href="registrar_sesion.php" class="btn btn-primary">Registrarse</a>
                 </form>
             </div>
         </div>
