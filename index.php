96da33036e0214dbe27bece462cbc21946f84d7f
Alan Brayan TV <al221911806@gmail.com>
2020-08-08 10:03:16 -0500

    Imagen index

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/all.min.css">
 </head>
-<body background="img/musica.jpg" class="img-fluid" alt="Responsive image">
+<body background="img/30.jpg" class="card bg-dark text-white" alt="Responsive image">
     <div class="container mt-3">
-        <div class="card">
+        <div class="card-img-overlay">
             <div class="card-header">
                 <i class="fa fa-heart"></i> Inicio de sesion
             </div>
@@ -24,6 +24,7 @@
                         <input type="password" class="form-control form-control-sm" id="contrasena" name="contrasena" required>
                     </div>
                     <button class="btn btn-primary btn-sm" type="submit"><i class="fa fa-save"></i> Enviar</button>
+                    <button class="btn btn-primary btn-sm" type="submit"><i class="fa fa-save"></i> Registrarse</button>
                 </form>
             </div>
         </div>
@@ -31,4 +32,6 @@
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
+</body>
 </html>
+
